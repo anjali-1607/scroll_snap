1267d84451984d07ea52b3095a71bb9c590192bd
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-09-09 13:41:45 +0530

    scroll

---
+++ src/Main.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <>
+      <div className="gallery">
+        <div className="gallery_item">
+          <img
+            className="gallery_image"
+            src="https://tinyjpg.com/images/social/website.jpg"
+            alt="flower"
+          />
+          <img
+            className="gallery_image"
+            src="https://onlinejpgtools.com/images/examples-onlinejpgtools/sunflower.jpg"
+            alt="flower"
+          />
+          <img
+            className="gallery_image"
+            src="https://wallpaperaccess.com/full/4723250.jpg"
+            alt="flower"
+          />
+          <img
+            className="gallery_image"
+            src="https://tinyjpg.com/images/social/website.jpg"
+            alt="flower"
+          />
+          <img
+            className="gallery_image"
+            src="https://onlinejpgtools.com/images/examples-onlinejpgtools/sunflower.jpg"
+            alt="flower"
+          />
+          <img
+            className="gallery_image"
+            src="https://wallpaperaccess.com/full/4723250.jpg"
+            alt="flower"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
